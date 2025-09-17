1ea5fbe233cb209bdfdb9885e6fe53b972e1ed35
BUBON <orelmarek@mail.ru>
2025-09-17 20:00:02 +0500

    w/ route-auth(reg/log) hook-form openAPI msw

--- src/features/auth/register.page.tsx
+++ src/features/auth/register.page.tsx
@@ -1,7 +1,21 @@
+import { ROUTES } from "@/shared/model/routes";
+import { Link } from "react-router-dom";
+import { AuthLayout } from "./ui/auth-layout";
+import { RegisterForm } from "./ui/register-form";
+
 function RegisterPage() {
-    return (
-        <div>RegisterPage</div>
-    )
+  return (
+    <AuthLayout
+      title="Регистрация"
+      description="Введите ваш email и пароль для регистрации в системе"
+      form={<RegisterForm />}
+      footerText={
+        <>
+          Уже есть аккаунт? <Link to={ROUTES.LOGIN}>Войти</Link>
+        </>
+      }
+    />
+  );
 }
 
-export const Component = RegisterPage
+export const Component = RegisterPage;
--- src/features/header/index.tsx
+++ src/features/header/index.tsx
@@ -1,5 +1,5 @@
 export const AppHeader = () => {
   return (
-    <div>AppHeader</div>
+    <main>AppHeader</main>
   )
 }
